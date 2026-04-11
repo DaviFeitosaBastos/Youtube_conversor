80ea249ad6335592af5cb4837c9332a362f28f6f
Davi Feitosa Bastos <davifeitosabastos@gmail.com>
2026-04-10 22:49:24 -0400

    feat: add get_logger to utils

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
 import sys
+import logging
+from rich.logging import RichHandler
 from pathlib import Path
+
 
 
 def get_base_dir():
     if hasattr(sys, '_MEIPASS'):
         return Path(sys.executable).parent
     return Path(__file__).parent
+
+
+def get_logger(name: str) -> logging.Logger:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
+    return logging.getLogger(name)
