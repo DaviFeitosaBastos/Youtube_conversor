ec3a9b7e2bfae422b6dc19e084207364e16dda64
Davi Feitosa Bastos <davifeitosabastos@gmail.com>
2026-04-15 00:52:41 -0400

    feat: added new function to service/video_service.py, now it's possible to see the video description and it was improved the user navegation

--- service/video_service.py
+++ service/video_service.py
@@ -10,23 +10,59 @@ FOLDER = get_base_dir() / "videos"
 FOLDER.mkdir(exist_ok=True)
 
 def get_video_info(url: str) -> None:
+    """  
+    Show to user all datas from an especified video like: views, likes, tittle, description etc...
+
+    Get all the data from URL that was catched by Pytubefix
     """
-    Fetches and displays video info.
-    """
+
     clear()
+
+    # Get data from the video url
     yt = YouTube(url)
-    cli.print(f"[bold]Title:[/bold] {yt.title}")
-    cli.print(f"[bold]Duration:[/bold] {yt.length}s")
-    cli.print(f"[bold]Views:[/bold] {yt.views}")
-    cli.print(f"[bold]Author:[/bold] {yt.author}")
-    cli.print(f"[bold]Rating:[/bold] {yt.rating}")
-    cli.print(f"[bold]Channel id:[/bold] {yt.channel_id}")
-    cli.print(f"[bold]Likes:[/bold] {yt.likes}\n")
+
+    def show_basic_info():
+        """  
+        Display: tittle | duration | views | author | channel ID | likes
+        """
+        cli.print(f"\n[bold]Title:[/bold] {yt.title}")
+        cli.print(
+            f"\n[bold]Duration:[/bold] " f"{f'{yt.length // 3600}:{(yt.length % 3600)//60:02d}:{yt.length % 60:02d}' if yt.length >= 3600 else f'{(yt.length % 3600)//60}:{yt.length % 60:02d}'}"
+        ) # Format the duration in seconds to [HH:MM:SS]
+        cli.print(f"\n[bold]Views:[/bold] {yt.views}")
+        cli.print(f"\n[bold]Author:[/bold] {yt.author}")
+        cli.print(f"\n[bold]Channel id:[/bold] {yt.channel_id}")
+        cli.print(f"\n[bold]Likes:[/bold] {yt.likes}\n")
+
+    # Loop for a better navegation :)
+    while True:
+        choice = input("Would you also like to see with the description? [y/n]: ").lower()
+
+        if choice == 'y':
+            show_basic_info()
+            cli.print(f"[bold]Description:[/bold] {yt.description}\n")
+            break
+
+        elif choice == 'n':
+            clear()
+            print("\nOkay! just the basic info\n")
+            show_basic_info()
+            break
+
+        else:
+            cli.print('[red]Just <y> or <n> are allowed')
+            sleep(1)
+            clear()
+
     if not yes_or_not("Type Y to return or N to quit "):
         exit()
+
+
     
 def download_high_res(url: str):
-    """Downloads the highest resolution video."""
+    """
+    Downloads the highest resolution video.
+    """
     yt = YouTube(url, on_progress_callback=on_progress)
     stream = yt.streams.get_highest_resolution()
     video_path = get_base_dir() / stream.default_filename
